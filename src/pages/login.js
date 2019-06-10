c2d3d1153db62da91f857c2de54f3303c89b468a
qaon <macycognizant@gmail.com>
2019-06-10 16:00:03 -0400

    change login v2

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useGlobal } from 'reactn'
 import ReactSVG from 'react-svg'
 import { makeStyles } from '@material-ui/core/styles'
@@ -7,12 +7,18 @@ import Fab from '@material-ui/core/Fab'
 import Grid from '@material-ui/core/Grid'
 import TextField from '@material-ui/core/TextField'
 import Typography from '@material-ui/core/Typography'
-import { withRouter } from "react-router-dom"
-import PasswordField from 'material-ui-password-field'
-import FormControl from '@material-ui/core/FormControl'
-import FormHelperText from '@material-ui/core/FormHelperText'
-import InputLabel from '@material-ui/core/InputLabel'
-// import PasswordField from '../components/PasswordField'
+import { withRouter } from 'react-router-dom'
+import accountOutline from '../assests/images/AccountOutline.svg'
+import keyVariant from '../assests/images/KeyVariant.svg'
+import blank from '../assests/images/Blank.svg'
+import Button from '@material-ui/core/Button'
+
+import NumberFormat from 'react-number-format';
+import MaskedInput from 'react-text-mask';
+import Input from '@material-ui/core/Input';
+
+// import Visibility from '@material-ui/icons/Visibility';
+// import VisibilityOff from '@material-ui/icons/VisibilityOff';
 
 const useStyles = makeStyles(theme => ({
     container: {
@@ -22,14 +28,12 @@ const useStyles = makeStyles(theme => ({
     textField: {
         marginLeft: theme.spacing(1),
         marginRight: theme.spacing(1),
+        width: '100%'
     },
     button: {
-        margin: theme.spacing(1),
-    },
-    fab: {
-        width: 123
-    },
-    passwordField: {
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
+        marginTop: theme.spacing(5),
         width: '100%'
     },
     formControl: {
@@ -37,16 +41,20 @@ const useStyles = makeStyles(theme => ({
         marginLeft: theme.spacing(1),
         marginRight: theme.spacing(1),
     }
-
 }));
 function Login (props) {
     const classes = useStyles()
     const [ userName, setUserName ] = useGlobal('userName')
+    const [ pin, setPin] = useState('')
+    
     function handleLogin () {
-        props.history.push("/salesZone");
+        props.history.push('/salesZone');
     }
-    function handleChange (e) {
+    function handleUserNameChange (e) {
         setUserName(e.target.value)
+    }
+    function handlePinChange(e) {
+        setPin(e.target.value.replace(/[^\d]/,''))
     }
     return (
         <div>
@@ -56,59 +64,49 @@ function Login (props) {
                 justify='center'
                 style={{ minHeight: '100vh' }} >
                 {/* row 1 */}
-                <Grid item xs={7} />
-                <Grid item xs={4} >
+                
+                <Grid item xs={10} >
                     <ReactSVG src={process.env.PUBLIC_URL + '/img/Macys_logo.svg'} />
-                    <Typography variant="h5">
+                    <Typography variant='h4'>
                         Full Service Selling
                     </Typography>
                 </Grid>
-                <Grid item xs={1} />
-                {/* row 2 */}
-                <Grid item xs={4} >
-                    <Typography variant="h4">
-                        Login
-                    </Typography>
-                </Grid>
-                <Grid item xs={8} />
+               
                 {/* row 3 */}
                 <Grid item xs={10} >
+                    
                     <form className={classes.container} noValidate autoComplete='off'>
-                        <TextField
-                            id='user-Name'
-                            label='Username'
-                            className={classes.textField}
-                            // value={values.name}
-                            onChange={handleChange}
-                            margin='normal'
-                            variant='filled'
-                            fullWidth={true}
-                        />
-                        
-                        
-                        <FormControl className={classes.formControl}>
-                        <InputLabel className={classes.textField} htmlFor='password-label'>Password</InputLabel>
-                        <PasswordField
-                            id='password-label'
-                            className={classes.passwordField}
-                            
-                        />
-                        
-                        </FormControl>
+                        <Grid container spacing={1} alignItems='flex-end'>
+                            <Grid item>
+                                <ReactSVG src={accountOutline} />
+                            </Grid>
+                            <Grid item xs={11}>
+                                <TextField id='associate-number' label='Associate number' className={classes.textField} onChange={handleUserNameChange}/>
+                            </Grid>
+                        </Grid>
+                        <Grid container spacing={1} alignItems='flex-end'>
+                            <Grid item>
+                                <ReactSVG src={keyVariant} />
+                            </Grid>
+                            <Grid item xs={11}>
+                           
+                                <TextField id='pin' label='Pin' className={classes.textField} type='password' onChange={handlePinChange} value={pin}/>
+        
+                            </Grid>
+                        </Grid>
+                        <Grid container spacing={1} alignItems='flex-end'>
+                            <Grid item>
+                                <ReactSVG src={blank} />
+                            </Grid>
+                            <Grid item xs={11}>
+                            <Button variant="contained" className={classes.button} onClick={() => handleLogin()} >
+                                Login
+                            </Button>
+                            </Grid>
+                        </Grid>
                     </form>
                 </Grid>
-                {/* row 4 */}
-                <Grid item xs={6} />
-                <Grid item xs={6}>
-                    <Fab 
-                        variant="extended" 
-                        color="primary" 
-                        aria-label="Login" 
-                        className={classNames(classes.margin, classes.fab)}
-                        onClick={() => handleLogin()} >
-                        Login
-                    </Fab>
-                </Grid>
+                
             </Grid>
         </div>
     )
